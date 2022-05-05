cdea9bace2cad630e041f3c161fa3a7f9b6fee54
llsahakyanll <sahakyansahak707@gmail.com>
2022-05-05 15:59:20 +0400

    hoga

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 console.log("Hello");
 $('#contactus').on('click',function(){
-    $('.section_home').removeClass('section_home_active');
     $('.section_contact').addClass('section_contact_active');
-    // $('.hsin_posa').toggleClass('hsin_posa_active')
+    $('.section_home').removeClass('section_home_active');
+    $('.section_footer').removeClass('section_footer_active')
 })
 $('#home').on('click',function(){
     $('.section_home').addClass('section_home_active');
     $('.section_contact').removeClass('section_contact_active');
-    // $('.hsin_posa').toggleClass('hsin_posa_active')
+    $('.section_footer').removeClass('section_footer_active')
+})
+$('#footer').on('click',function(){
+    $('.section_footer').addClass('section_footer_active')
+    $('.section_home').removeClass('section_home_active');
+    $('.section_contact').removeClass('section_contact_active');
 })
